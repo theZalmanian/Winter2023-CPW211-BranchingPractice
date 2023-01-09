f70b93f4b16070d81ed4e9998ab07cc224f43428
Zalman Izak <thezalmanian@gmail.com>
2023-01-08 19:06:28 -0800

    - Changed form title -- Changed form background color

--- BranchingPractice/Form1.Designer.cs
+++ BranchingPractice/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(478, 333);
+            this.Name = "Form1";
+            this.Text = "Branching Practice";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
